132374b7f34a00fa61687b511a0031e9084618cf
Alain LEGLISE <alainleglise@outlook.fr>
2023-08-18 10:02:37 +0200

    Initialize git project - FakeASO

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card, Input, message, Space } from 'antd';
+import { SizeType } from 'antd/es/config-provider/SizeContext';
+import React, { useState } from 'react';
+
+// import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const info = () => {
+        messageApi.open({content: 'Hello, Ant Design!',
+            type: 'info',
+            duration: 60}
+        );
+    };
+
+    const [size, setSize] = useState<SizeType>('large'); // default is 'middle'
+    return (
+        <Card style={{width: 300}}>
+            {contextHolder}
+            <Space direction="vertical">
+                <Input.Password id={"password"} placeholder="input password"/>
+            </Space>
+            <Button id={"okButton0"} type="primary" size={size}
+                    onClick={info}
+            >OK</Button>
+        </Card>
+    );
 }
 
 export default App;
